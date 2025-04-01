a4cbeca75ac9007671c527a7a358baf27a9c56db
James Lynch <jdlynch@lakeheadu.ca>
2025-04-01 14:33:01 -0400

    Started trying to integrate the phi-4 model into the simulation

--- JLynch_A3_Part1.py
+++ JLynch_A3_Part1.py
@@ -14,6 +14,25 @@ class HeroAgent(CellAgent):
     def __init__(self, model):
 
         super().__init__(model)
+    def step(self):
+        self.move()
+    def move(self):
+        message = self.getEffects()
+        output = self.model.PromptModel("You are a hero in a simulation that is looking to find the gold to finish the game, but there are obstacles in your way. There are pits and the wumpus if you encounter either you fail and die, but you can tell if they are nearby by effects they leave on the neighboring tiles around them. Pits create a breeze and Wumpus creates a foul smell. "
+        "The gold also creates an effect with leaving glitter nearby so if you encounter these effects think carefully about your next step.", message, "Your thoughts: ")
+        print(output)
+    def get_Effects(self):
+        message = ""
+        effects = self.model.effects[self.pos]
+        if effects["smell"]:
+            message += "You smell a foul smell coming from a neighbouring tile."
+        if effects["breeze"]:
+            message += "You feel a breeze coming from a neighbouring tile"
+        if effects["glitter"]:
+            message += "You see some glitter on the ground"
+        if not effects:
+            message = "There are no effects on this cell"
+        return effects
 
 class WumpusAgent(FixedAgent):
     def __init__(self, model):
@@ -61,6 +80,29 @@ class WumpusModel(mesa.Model):
         place_agents(WumpusAgent, wumpus)
         place_agents(GoldAgent, gold)
 
+        if torch.cuda.is_available():
+            for i in range(torch.cuda.device_count()):
+                print(i, torch.cuda.get_device_properties(i))
+
+        torch.random.manual_seed(0)
+
+        model_path = "microsoft/Phi-4-mini-instruct"
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            device_map="cpu", # "cpu" or "auto" or "cuda:0" for cuda device 0, 1, 2, 3 etc. if you have multiple GPUs
+            torch_dtype="auto",
+            trust_remote_code=True
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+        #gotta have a tokenizer for each model otherwise the token mappings won't match
+        self.pipe = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer
+        )
+
     def update_effects(self, pos, agentType):
         neighbors = self.grid.get_neighborhood(pos, moore=False, include_center=False)
         for n in neighbors:
@@ -70,6 +112,35 @@ class WumpusModel(mesa.Model):
                 self.effects[n]["glitter"] = True
             elif agentType is WumpusAgent:
                 self.effects[n]["smell"] = True
+    def PromptModel(self, context, memorystream, prompt):
+        
+        #lower temperature generally more predictable results, you can experiment with this
+        generation_args = {
+            "max_new_tokens": 64,
+            "return_full_text": False,
+            "temperature": 0.0,
+            "do_sample": False,
+        }
+
+        llmprompt = prompt
+        
+        messages = [
+            {"role": "system", "content": context},
+            {"role": "system", "content": memorystream},
+            {"role": "user", "content": llmprompt},
+        ]
+
+        #time1 = int(round(time.time() * 1000))
+
+        output = self.pipe(messages, **generation_args)
+        return output[0]['generated_text']
+
+        #time2 = int(round(time.time() * 1000))
+        #print("Generation time: " + str(time2 - time1))
+        #self.datacollector.collect(self)
+    def step(self):
+        self.agents_by_type[HeroAgent].shuffle_do("step")
+
 
 def wumpus_portrayal(agent):
     if agent is None:
